614e24af0095c389453b5b89e0e943b0d5d2d9bd
DanijelRadakovic <danijelradakovic1996@gmail.com>
2019-02-03 21:48:53 +0100

    merge

---
+++ src/main/java/zis/rs/zis/service/nonProcessService/LekarServis.java
@@ -0,0 +1,16 @@
+package zis.rs.zis.service.nonProcessService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import zis.rs.zis.repository.xml.LekarXMLRepozitorijum;
+
+@Service
+public class LekarServis {
+
+    @Autowired
+    private LekarXMLRepozitorijum lekarXMLRepozitorijum;
+
+    public String dobaviSve() { return lekarXMLRepozitorijum.dobaviSve();}
+
+    public String pretragaPoId(String id){ return lekarXMLRepozitorijum.pretragaPoId(id);}
+}
